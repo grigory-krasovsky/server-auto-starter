699f588afbf3a9f42146f4b21c3b2bf1261f9833
krasovsky <krasovsky@elbrus.ru>
2026-04-04 10:52:08 +0300

    initial

---
+++ src/main/java/com/example/serverautostarter/ServerAutoStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverautostarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAutoStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerAutoStarterApplication.class, args);
+    }
+
+}
